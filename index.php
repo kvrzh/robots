7ac09033939cf68c7819bd633d950228ec938483
Anton <kovr-anton@mail.ru>
2016-09-15 15:40:07 +0300

    version 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Главная</title>
-
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
 <form method="post" action="">
@@ -15,10 +15,50 @@
 
 <?php
 include 'robots.php';
+include 'rules.php';
 if (isset($_POST['go']) && isset($_POST['url'])) {
     $robot = new robots($_POST['url']);
-    print_r($robot->getResult());
-    print_r($robot->getUrl());
+    $result = $robot->getResult();
 }
 ?>
+<table>
+
+    <tr>
+        <td>Проверка наличия файла robots.txt</td>
+        <td><?php if ($result['isExists'] === true) echo '<span class="success">Ок</span>'; else echo '<span class="error">Ошибка</span>' ?></td>
+        <td><?php if ($result['isExists'] === true) echo '<span class="success">' . $rules['isExists']['true']['Состояние'] . '</span>'; else echo '<span class="error">' . $rules['isExists']['false']['Состояние'] . '</span>' ?></td>
+        <td><?php if ($result['isExists'] === true) echo '<span class="success">' . $rules['isExists']['true']['Рекомендации'] . '</span>'; else echo '<span class="error">' . $rules['isExists']['false']['Рекомендации'] . '</span>' ?></td>
+    </tr>
+    <tr>
+        <td>Проверка указания директивы Host</td>
+        <td><?php if ($result['isHost'] === true) echo '<span class="success">Ок</span>'; else echo '<span class="error">Ошибка</span>' ?></td>
+        <td><?php if ($result['isHost'] === true) echo '<span class="success">' . $rules['isHost']['true']['Состояние'] . '</span>'; else echo '<span class="error">' . $rules['isHost']['false']['Состояние'] . '</span>' ?></td>
+        <td><?php if ($result['isHost'] === true) echo '<span class="success">' . $rules['isHost']['true']['Рекомендации'] . '</span>'; else echo '<span class="error">' . $rules['isHost']['false']['Рекомендации'] . '</span>' ?></td>
+    </tr>
+    <tr>
+        <td>Проверка количества директив Host, прописанных в файле</td>
+        <td><?php if ($result['countHost'] == 1) echo '<span class="success">Ок</span>'; else echo '<span class="error">Ошибка</span>' ?></td>
+        <td><?php if ($result['countHost'] == 1) echo '<span class="success">' . $rules['countHost']['true']['Состояние'] . '</span>'; else echo '<span class="error">' . $rules['countHost']['false']['Состояние'] . '</span>' ?></td>
+        <td><?php if ($result['countHost'] == 1) echo '<span class="success">' . $rules['countHost']['true']['Рекомендации'] . '</span>'; else echo '<span class="error">' . $rules['countHost']['false']['Рекомендации'] . '</span>' ?></td>
+    </tr>
+    <tr>
+        <td>Проверка размера файла robots.txt</td>
+        <td><?php if ($result['size'] <= 256000) echo '<span class="success">Ок</span>'; else echo '<span class="error">Ошибка</span>' ?></td>
+        <td><?php if ($result['size'] <= 256000) echo '<span class="success">' . $rules['size']['true']['Состояние'] . '</span>'; else echo '<span class="error">' . $rules['size']['false']['Состояние'] . '</span>' ?></td>
+        <td><?php if ($result['size'] <= 256000) echo '<span class="success">' . $rules['size']['true']['Рекомендации'] . '</span>'; else echo '<span class="error">' . $rules['size']['false']['Рекомендации'] . '</span>' ?></td>
+    </tr>
+    <tr>
+        <td>Проверка указания директивы Sitemap</td>
+        <td><?php if ($result['isSitemap'] === true) echo '<span class="success">Ок</span>'; else echo '<span class="error">Ошибка</span>' ?></td>
+        <td><?php if ($result['isSitemap'] === true) echo '<span class="success">' . $rules['isSitemap']['true']['Состояние'] . '</span>'; else echo '<span class="error">' . $rules['isSitemap']['false']['Состояние'] . '</span>' ?></td>
+        <td><?php if ($result['isSitemap'] === true) echo '<span class="success">' . $rules['isSitemap']['true']['Рекомендации'] . '</span>'; else echo '<span class="error">' . $rules['isSitemap']['false']['Рекомендации'] . '</span>' ?></td>
+    </tr>
+    <tr>
+        <td>Проверка кода ответа сервера для файла robots.txt</td>
+        <td><?php if ($result['code'] == 200) echo '<span class="success">Ок</span>'; else echo '<span class="error">Ошибка</span>' ?></td>
+        <td><?php if ($result['code'] == 200) echo '<span class="success">' . $rules['code']['true']['Состояние'] . '</span>'; else echo '<span class="error">' . $rules['code']['false']['Состояние'] . '</span>' ?></td>
+        <td><?php if ($result['code'] == 200) echo '<span class="success">' . $rules['code']['true']['Рекомендации'] . '</span>'; else echo '<span class="error">' . $rules['code']['false']['Рекомендации'] . '</span>' ?></td>
+    </tr>
+
+</table>
 </body>
